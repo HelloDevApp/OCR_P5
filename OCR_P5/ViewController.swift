d981f7ae50567c152d861c16481ed78ba3d42624
dylan <nais95100@icloud.com>
2018-09-09 22:38:05 +0200

    Initial Commit

---
+++ OCR_P5/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OCR_P5
+//
+//  Created by Mac Book Pro on 09/09/2018.
+//  Copyright © 2018 dylan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
