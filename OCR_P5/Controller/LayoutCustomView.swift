49c09638c183f659ca4a586004d664063c10b4a9
dylan <nais95100@icloud.com>
2018-09-18 17:18:55 +0200

    added the swift file that will manage the layout

---
+++ OCR_P5/Controller/LayoutCustomView.swift
@@ -0,0 +1,21 @@
+//
+//  LayoutCustomView.swift
+//  OCR_P5
+//
+//  Created by Mac Book Pro on 18/09/2018.
+//  Copyright © 2018 dylan. All rights reserved.
+//
+
+import UIKit
+
+class LayoutCustomView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
