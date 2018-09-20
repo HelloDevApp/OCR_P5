fb2f3fcdc565615dcda0f7f9e55c7813d4c21c04
dylan <nais95100@icloud.com>
2018-09-20 17:03:29 +0200

    creation of new connections between the objects of the builder interface and the view controller + addition of the method that will display the chosen layout and addition of the 3 actions that allow to choose the layout according to the button pressed

--- OCR_P5/Controller/ViewController.swift
+++ OCR_P5/Controller/ViewController.swift
@@ -8,8 +8,52 @@
 
 import UIKit
 
+//--------------------------------------------------
+// MARK: - Enumeration
+//-------------------------------------------------
+enum Layout {
+    case rectangularBottomView
+    case rectangularTopView
+    case squareView
+}
+
+
 class ViewController: UIViewController {
 
+    //--------------------------------------------------
+    // MARK: - @IBOutlets
+    //--------------------------------------------------
+    
+    //Top StackView
+    @IBOutlet weak var stackViewTop: UIStackView!
+    //Top Left squareView
+    // ⚠️1 = Top    ⚠️2 = Bottom    ⚠️L = Left    ⚠️R = Right
+    @IBOutlet weak var squareViewL1: UIView!
+    //Top Left imageView
+    @IBOutlet weak var imageViewL1: UIImageView!
+    //Top Right squareView
+    @IBOutlet weak var squareViewR1: UIView!
+    //Top Right squareView
+    @IBOutlet weak var imageViewR1: UIImageView!
+    //--------------------------------------------------
+    
+    //Bottom StackView
+    @IBOutlet weak var stackViewBottom: UIStackView!
+    //Bottom Left squareView
+    // ⚠️1 = Top    ⚠️2 = Bottom    ⚠️L = Left    ⚠️R = Right
+    @IBOutlet weak var squareViewL2: UIView!
+    //Bottom Left imageView
+    @IBOutlet weak var imageVieWL2: UIView!
+    //Bottom Right squareVIew
+    @IBOutlet weak var squareViewR2: UIView!
+    //Bottom Right imageView
+    @IBOutlet weak var imageViewR2: UIView!
+    
+    
+    //--------------------------------------------------
+    // MARK: - Standard Methods
+    //-------------------------------------------------
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +63,42 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
+    
+    
+    //--------------------------------------------------
+    // MARK: - Layout Buttons @IBAction
+    //-------------------------------------------------
+    
+    //layoutButton is on the left in portrait mode ⚠️layoutButton is on the top in landscape mode⚠️
+    @IBAction func layoutButton1Tapped(_ sender: UIButton) {
+    }
+    //center layoutButton in portrait and landscape mode
+    @IBAction func layoutButton2Tapped(_ sender: UIButton) {
+        displaysLayout(layoutStyle: .rectangularBottomView)
+    }
+    //layoutButton is on the right in portrait mode ⚠️layoutButton is on the bottom in landscape mode⚠️
+    @IBAction func layoutButtonTapped(_ sender: UIButton) {
+    }
+    
+    
+    
+    //-----------------------------------------------------
+    // MARK: - Settings
+    //-----------------------------------------------------
+    
+    func displaysLayout(layoutStyle: Layout) {
+        switch layoutStyle {
+        case .rectangularBottomView:
+            squareViewR2.isHidden = true
+            print(squareViewR2.isHidden)
+            
+        case .rectangularTopView:
+            break
+        case .squareView:
+            break
+        }
+    }
 }
 
