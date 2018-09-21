3225cc57ce23bc1cc5da330fcb1ff48d05d5ceb0
dylan <nais95100@icloud.com>
2018-09-21 11:50:11 +0200

    finished implementing the displaysLayout() method, modified the disabledButton() method, new constraints for stack views in the middle, added the 3 layoutButton properties

--- OCR_P5/Controller/ViewController.swift
+++ OCR_P5/Controller/ViewController.swift
@@ -21,7 +21,7 @@ enum Layout {
 class ViewController: UIViewController {
 
     //--------------------------------------------------
-    // MARK: - @IBOutlets
+    // MARK: - @IBOutlets: StackViewTop and Bottom
     //--------------------------------------------------
     
     //Top StackView
@@ -49,6 +49,29 @@ class ViewController: UIViewController {
     //Bottom Right imageView
     @IBOutlet weak var imageViewR2: UIView!
     
+    //--------------------------------------------------
+    // MARK: - @IBOutlets: LayoutButton
+    //--------------------------------------------------
+    //PORTRAIT MODE: ⚠️1 = Left   ⚠️2 = Center   ⚠️2 = Right
+    //LANDSCAPE MODE: ⚠️1 = Top   ⚠️2 = Center   ⚠️2 = Bottom
+    @IBOutlet weak var layoutButton1: UIButton!
+    @IBOutlet weak var layoutButton2: UIButton!
+    @IBOutlet weak var layoutButton3: UIButton!
+    
+    
+    //--------------------------------------------------
+    // MARK: - Properties
+    //-------------------------------------------------
+    
+    
+    
+    //--------------------------------------------------
+    // MARK: - Constraints
+    //-------------------------------------------------
+    
+    //constraint use to have a square view
+    @IBOutlet weak var squareViewL2Ratio: NSLayoutConstraint!
+    @IBOutlet weak var squareViewL1Ratio: NSLayoutConstraint!
     
     //--------------------------------------------------
     // MARK: - Standard Methods
@@ -57,6 +80,10 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        squareViewL1Ratio.isActive = false
+        squareViewL2Ratio.isActive = false
+        displaysLayout(layoutStyle: .rectangularBottomView)
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -73,13 +100,18 @@ class ViewController: UIViewController {
     
     //layoutButton is on the left in portrait mode ⚠️layoutButton is on the top in landscape mode⚠️
     @IBAction func layoutButton1Tapped(_ sender: UIButton) {
+        disabledButton(disabled: layoutButton1, enabled1: layoutButton2, enabled2: layoutButton3)
+        displaysLayout(layoutStyle: .rectangularTopView)
     }
     //center layoutButton in portrait and landscape mode
     @IBAction func layoutButton2Tapped(_ sender: UIButton) {
+        disabledButton(disabled: layoutButton2, enabled1: layoutButton1, enabled2: layoutButton3)
         displaysLayout(layoutStyle: .rectangularBottomView)
     }
     //layoutButton is on the right in portrait mode ⚠️layoutButton is on the bottom in landscape mode⚠️
-    @IBAction func layoutButtonTapped(_ sender: UIButton) {
+    @IBAction func layoutButton3Tapped(_ sender: UIButton) {
+        disabledButton(disabled: layoutButton3, enabled1: layoutButton1, enabled2: layoutButton2)
+        displaysLayout(layoutStyle: .squareView)
     }
     
     
@@ -89,16 +121,39 @@ class ViewController: UIViewController {
     //-----------------------------------------------------
     
     func displaysLayout(layoutStyle: Layout) {
+        
         switch layoutStyle {
-        case .rectangularBottomView:
-            squareViewR2.isHidden = true
-            print(squareViewR2.isHidden)
             
+            
+        case .rectangularBottomView:
+
+            squareViewR2.isHidden = true
+            imageViewR2.isHidden = true
+            squareViewR1.isHidden = false
+            imageViewR1.isHidden = false
+  
         case .rectangularTopView:
-            break
+
+            squareViewR1.isHidden = true
+            imageViewR1.isHidden = true
+            squareViewR2.isHidden = false
+            imageViewR2.isHidden = false
+            
         case .squareView:
-            break
+
+            squareViewR1.isHidden = false
+            imageViewR1.isHidden = false
+            squareViewR2.isHidden = false
+            imageViewR2.isHidden = false
+            
         }
+    }
+    func disabledButton(disabled: UIButton, enabled1: UIButton, enabled2: UIButton) {
+        
+        disabled.isEnabled = false
+        enabled1.isEnabled = true
+        enabled2.isEnabled = true
+        
     }
 }
 
